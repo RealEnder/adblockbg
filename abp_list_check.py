60675e636d9d62faa91e00a91dade96aedf4fcf9
Alex Stanev <alex@stanev.org>
2011-07-21 14:05:06 +0000

    pylint fixes for abp_list_check. Version bump to 0.2.2

--- abp_list_check.py
+++ abp_list_check.py
@@ -2,98 +2,96 @@
 # The source code is distributed under GPLv3 license
 import sys
 import os.path
-import string
 import urlparse
 import socket
 import urllib2
 
-print 'Check AdBlockPlus filters for outdated entries v0.2.1 by Alex Stanev, http://stanev.org/abp'
+print 'Check AdBlockPlus filters for outdated entries v0.2.2 by Alex Stanev, http://stanev.org/abp'
 
-if len(sys.argv) == 2:
-    if os.path.exists(sys.argv[1]):
-        socket.setdefaulttimeout(5)
-        no_res = 0
-        no_host = 0
-        skip = 0
-        short = 0
-        curr = 0
-        
-        
-        abplist = open(sys.argv[1])
-        for line in abplist:
-            curr = curr + 1
-            rline = line.strip()
-            
-            #check for comment or empty ot section [
-            if len(rline) == 0:
-                skip = skip + 1
-                continue
-            if rline[0] in ('[','!'):
-                skip = skip + 1
-                continue
-            
-            # remove #, $, ~, ^ if present
-            if rline.find('#') <> -1:
-                rline = rline[:rline.find('#')]
-            if rline.find('$') <> -1:
-                rline = rline[:rline.find('$')]
-            if rline.find('~') <> -1:
-                rline = rline[:rline.find('~')]
-            if rline.find('^') <> -1:
-                rline = rline[:rline.find('^')]
-            
-            #check for short entries
-            if len(rline) < 4:
-                short = short + 1
-                print curr,': Too short :',line,
-                continue
+if len(sys.argv) != 2:
+    print 'Usage: abp_list_check.py [abp_list.txt]'
+    sys.exit(1)
+if not os.path.exists(sys.argv[1]):
+    print 'Could not find the list'
+    sys.exit(1)
+
+socket.setdefaulttimeout(5)
+no_res = 0
+no_host = 0
+skip = 0
+short = 0
+curr = 0
+
+abplist = open(sys.argv[1])
+for line in abplist:
+    curr += 1
+    rline = line.strip()
     
-            #check whitelists too
-            if rline[0:1] == '@@':
-                rline = rline[2:]
+    #check for comment or empty ot section [
+    if len(rline) == 0:
+        skip += 1
+        continue
+    if rline[0] in ('[','!'):
+        skip += 1
+        continue
+    
+    # remove #, $, ~, ^ if present
+    if rline.find('#') != -1:
+        rline = rline[:rline.find('#')]
+    if rline.find('$') != -1:
+        rline = rline[:rline.find('$')]
+    if rline.find('~') != -1:
+        rline = rline[:rline.find('~')]
+    if rline.find('^') != -1:
+        rline = rline[:rline.find('^')]
+    
+    #check for short entries
+    if len(rline) < 4:
+        short += 1
+        print curr, ': Too short :', line,
+        continue
 
-            #remove single or double starting pipe
-            if rline[0] == '|':
-                rline = rline[1:]
-            if rline[0] == '|':
-                rline = rline[1:]
-                
-            #check for protocol idents
-            if rline[0:7].lower() == 'http://' or rline[0:8].lower() == 'https://':
-                print curr,': Consider removing protocol identificator :',line,
-            else:
-                rline='http://'+rline
-            
-            url = urlparse.urlparse(rline)
-            
-            #check for wildcards in host
-            if url[1] == '' or url[1].find('*') <> -1:
-                no_host = no_host + 1
-                #print curr,': Wildcard or missing host :',line,
-                continue
-            
-            #remove wildcards in path if present
-            path = url[2]
-            if len(path) > 1:
-                while path.endswith('.'):
-                    path = path[:path.rfind('/')]
-                while path.rfind('*') <> -1:
-                    path = path[:path.rfind('*')]
-                    path = path[:path.rfind('/')]
-            
-            #access the resource
-            try:
-                resp = urllib2.urlopen(url[0]+'://'+url[1]+path);
-            except Exception, e:
-                if hasattr(e, 'code'):
-                    if e.code == 404:
-                        no_res = no_res + 1
-                        print curr,': Resource not found :',line,
-                        
-        abplist.close()
-        print '\nChecked lines:%s\nNot found:%s\nIndeterminable:%s\nToo short:%s\nSkipped:%s' %(curr, no_res, no_host, short, skip)
-        sys.exit(0)
+    #check whitelists too
+    if rline[0:1] == '@@':
+        rline = rline[2:]
+
+    #remove single or double starting pipe
+    if rline[0] == '|':
+        rline = rline[1:]
+    if rline[0] == '|':
+        rline = rline[1:]
         
-print 'Usage: abp_list_check.py [abp_list.txt]'
-sys.exit(1)
-
+    #check for protocol idents
+    if rline[0:7].lower() == 'http://' or rline[0:8].lower() == 'https://':
+        print curr, ': Consider removing protocol identificator :', line,
+    else:
+        rline = 'http://' + rline
+    
+    url = urlparse.urlparse(rline)
+    
+    #check for wildcards in host
+    if url[1] == '' or url[1].find('*') != -1:
+        no_host += 1
+        #print curr, ': Wildcard or missing host :', line,
+        continue
+    
+    #remove wildcards in path if present
+    path = url[2]
+    if len(path) > 1:
+        while path.endswith('.'):
+            path = path[:path.rfind('/')]
+        while path.rfind('*') != -1:
+            path = path[:path.rfind('*')]
+            path = path[:path.rfind('/')]
+    
+    #access the resource
+    try:
+        resp = urllib2.urlopen(url[0]+'://'+url[1]+path)
+    except Exception, e:
+        if hasattr(e, 'code'):
+            if e.code == 404:
+                no_res += 1
+                print curr, ': Resource not found :', line,
+                
+abplist.close()
+print '\nChecked lines:%s\nNot found:%s\nIndeterminable:%s\nToo short:%s\nSkipped:%s' % (curr, no_res, no_host, short, skip)
